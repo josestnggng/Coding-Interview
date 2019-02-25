328deff02271a42ba7ab11add834b2a49d477b5a
Jose Sitanggang <jose.stnggng@gmail.com>
2019-02-26 01:22:13 +0700

    flip bit, O(n)

--- palidrome_permutation.py
+++ palidrome_permutation.py
@@ -6,19 +6,13 @@ def palidrome_permutation(string):
         if c == ' ':
             continue
         if c in dict_char.keys():
-            dict_char[c] += 1
+            dict_char[c] = 0
         else:
             dict_char[c] = 1
         no_space_len += 1
 
-    max_odd = no_space_len % 2
-    count_odd = 0
-    for x in dict_char.values():
-        if x % 2 == 1:
-            count_odd += 1
-        if count_odd > max_odd:
-            return False
-
+    if no_space_len % 2 != sum(dict_char.values()):
+        return False
     return True
 
 
